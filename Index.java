743541f3e62b70c13f1914598ed81b887c983f36
khairul umam albi <81983048+albikhairul212@users.noreply.github.com>
2021-11-16 09:39:25 +0700

    code v2 pada index dan Page1
    menambahkan code pada index dan Page1

--- Index.java
+++ Index.java
@@ -1,6 +1,27 @@
 public class Index{
     
+    private String nama;
+    private int Pass;
 
+
+    public String getNama(){
+        return this.nama;
+    }
+
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+
+
+
+    public int getPass(){
+        return this.Pass;
+    }
+
+    public void setPass(int Pass){
+        this.Pass = Pass; 
+    }
     
+
 
 }
