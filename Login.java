76d9c2bb4343bb5ac55164f359ecd8d0232b715b
khairul umam albi <81983048+albikhairul212@users.noreply.github.com>
2021-11-18 23:30:40 +0700

    v1

--- Login.java
+++ Login.java
@@ -1,34 +1,3 @@
-public class Login{
-
-
-
-    public Login(){
-
-    }
-
-
-    private String nama;
-    private int Pass;
-
-
-    public String getNama(){
-        return this.nama;
-    }
-
-    public void setNama(String nama){
-        this.nama = nama;
-    }
-
-
-
-    public int getPass(){
-        return this.Pass;
-    }
-
-    public void setPass(int Pass){
-        this.Pass = Pass; 
-    }
-
-
-
+public class Login {
+    
 }
