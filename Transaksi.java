0ae72f668da92e5c03b7ffbb9a5a5d4e681ccb4e
Teuku-Nabil <nabilmuhammad246@gmail.com>
2021-11-25 01:31:56 +0700

    Update Transaksi.java

--- Transaksi.java
+++ Transaksi.java
@@ -1,50 +1,36 @@
-public class Transaksi extends TampilanAwal{
+public class transaksi extends TampilanAwal{
 
-    protected String idTransaksi;
-    protected String idPembeli;
-    private String kodeResi;
-    private int jumlahBarang;
+    private String idTransaksi;
+    private String idPembeli;
     private double uang;
     private double bayar;
-    private double harga;
-    
+    private double HargaBarang;
+    private int JumlahBarang;
 
-    public Transaksi(){
-        this.idTransaksi = idTransaksi; this.idPembeli = idPembeli;
+    public String Transaksi(){
+        return idTransaksi;
+        return idPembeli; 
     }
 
-    public Transaksi(String idTransaksi, String idPembeli){
+    public void Transaksi(String idTransaksi, String idPembeli){
         this.idTransaksi = idTransaksi;
         this.idPembeli = idPembeli;
     }
 
-    public String getKodeResi(){
-        return kodeResi;
+    public Double getHargaBarang(){
+        return HargaBarang;
     }
 
-    public void setKodeResi(){
-        this.kodeResi = kodeResi;
+    public int getJumlahBarang(){
+        return JumlahBarang;
     }
 
-    public int getJmlBarang(){
-        return jumlahBarang;
-    }
-
-    public void setJmlBarang(int jumlah){
-        this.jumlahBarang = jumlah;
-    }
-
-    public void totalHarga(){ 
-        
-        harga = hargaBarang1 * jumlahBarang;
+    public Double totalHarga(){
+        return this.getHargaBarang() * this.getJumlahBarang();
     }
 
     protected void setBayar(double uang){
-        if(uang > harga){
-            double total = uang - harga;
-        }else{
-            System.out.println("Uang yang anda masukkan kurang");
-        }
+        this.bayar = uang;
     }
 
 }
