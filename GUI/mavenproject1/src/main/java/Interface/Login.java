b3b3a403d4db77b1d78dde92d2da578bc4445d54
khairul umam albi <81983048+albikhairul212@users.noreply.github.com>
2021-11-25 22:57:24 +0700

    upda

--- GUI/mavenproject1/src/main/java/Interface/Login.java
+++ GUI/mavenproject1/src/main/java/Interface/Login.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package Interface;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 //import java.awt.*;
 //import javax.swing.*;
 /**
@@ -15,7 +17,16 @@ public class Login extends javax.swing.JFrame {
      * Creates new form Login
      */
     public Login() {
+        
         initComponents();
+
+        Dimension layar = Toolkit.getDefaultToolkit().getScreenSize();
+
+        // membuat titik x dan y
+        int x = layar.width / 2  - this.getSize().width / 2;
+        int y = layar.height / 2 - this.getSize().height / 2;
+
+        this.setLocation(x, y);
     }
 
     /**
