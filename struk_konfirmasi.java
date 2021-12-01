dc724d751e65c6fbc2a15a6848c2107d3041fd3f
FaridaMandani <93516581+FaridaMandani@users.noreply.github.com>
2021-12-01 11:37:11 +0700

    Update struk_konfirmasi.java

--- struk_konfirmasi.java
+++ struk_konfirmasi.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 public class struk_konfirmasi {
 
     private double Produk;
@@ -36,4 +37,77 @@ public class struk_konfirmasi {
     }
      
 
+=======
+public class struk_konfirmasi extends Transaksi {
+
+   private double Produk;
+   private double OngkosKirim;
+   private double BiayaPacking;
+   private double Asurasi;
+   private String idTransaksi;
+   private String idPembeli;
+   private int JumlahBarang;
+   private double HargaBarang;
+   private String COD;
+   private String TransferBank;
+
+   public class TotalBiaya{
+
+        public String Transaksi(){
+            return idTransaksi;
+            return idPembeli; 
+        }
+
+        public void Transaksi(String idTransaksi, String idPembeli){
+            this.idTransaksi = idTransaksi;
+            this.idPembeli = idPembeli;
+        }
+        public void getProduk(){
+            return Produk;
+        }
+
+        public void setProduk(double Produk){
+            this.Produk = Produk;
+        }
+
+        public void getBiayaPacking(){
+            return BiayaPacking;
+        }
+
+        public void setBiayaPacking(double BiayaPacking){
+            this.BiayaPacking = BiayaPacking;
+        }
+
+        public void getAsuransi(){
+            return Asuransi;
+        }
+
+        public void setAsurasi(double Asuransi){
+            this.Asuransi = Asuransi;
+        }
+
+        public Double Total(){
+            return this.getHargaBarang() * this.getJumlahBarang();
+        }
+    }
+
+    public class MetodePembayaran{
+
+        public String getCOD(){
+            return COD;
+        }
+
+        public void setCOD(String CoD){
+            this.COD = COD;
+        }
+
+        public String getTransferBank(){
+            return TransferBank;
+        }
+
+        public void setTransferBank(String TransferBank){
+            this.TransferBank = TransferBank;
+        }
+    } 
+>>>>>>> Stashed changes
 }
