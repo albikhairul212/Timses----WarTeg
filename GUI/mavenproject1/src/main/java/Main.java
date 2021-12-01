e698aa7a710578e8bd08fcff8c44ea5d981885ed
khairul umam albi <81983048+albikhairul212@users.noreply.github.com>
2021-12-01 21:35:48 +0700

    finally

---
+++ GUI/mavenproject1/src/main/java/Main.java
@@ -0,0 +1,26 @@
+
+import Interface.Login;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author UMAM
+ */
+public class Main {
+
+    
+    public static void main(String[] args) {
+        
+
+        Login l1 = new Login();
+
+        l1.setVisible(true);
+
+    }
+    
+    
+}
