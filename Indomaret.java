4005e3f3ee70c86f64e74cbd64293b31ad1cb0d8
khairul umam albi <81983048+albikhairul212@users.noreply.github.com>
2021-12-01 21:24:34 +0700

    exit

--- Indomaret.java
+++
@@ -1,9 +0,0 @@
-public class Indomaret extends Transaksi{
-
-    private double uang;
-
-    public Indomaret(String idTransaksi, String idPembeli, double uang){
-        super(idTransaksi, idPembeli);
-        setBayar(uang);
-    }
-}
--- TransferBank.java
+++
@@ -1,9 +0,0 @@
-public class TransferBank extends Transaksi{
-    
-    private double uang;
-
-    public TransferBank(String idTransaksi, String idPembeli, double uang){
-        super(idTransaksi, idPembeli);
-        setBayar(uang);
-    }
-}
