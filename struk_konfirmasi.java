1ce8bd0736f80eaca51b65fda0b2d2b137734952
FaridaMandani <93516581+FaridaMandani@users.noreply.github.com>
2021-12-01 11:40:30 +0700

    Update struk_konfirmasi.java

--- struk_konfirmasi.java
+++ struk_konfirmasi.java
@@ -1,113 +1,61 @@
-<<<<<<< Updated upstream
-public class struk_konfirmasi {
+public class Struk_Konfirmasi {
 
     private double Produk;
     private double OngkosKirim;
     private double BiayaPacking;
-    private double Asurasi;
+    private double Asuransi;
     private String COD;
     private String TransferBank;
+    private double Total;
 
-    public void getProduk(){
-        return Produk;
+    
+    public double getProduk(){
+        return this.Produk;
     }
 
     public void setProduk(double Produk){
         this.Produk = Produk;
     }
 
-    public void getBiayaPacking(){
-        return BiayaPacking;
+    public double getBiayaPacking(){
+        return this.BiayaPacking;
     }
 
     public void setBiayaPacking(double BiayaPacking){
         this.BiayaPacking = BiayaPacking;
     }
 
-    public void getAsuransi(){
-        return Asuransi;
+    public double getAsuransi(){
+        return this.Asuransi;
     }
 
     public void setAsurasi(double Asuransi){
         this.Asuransi = Asuransi;
     }
 
-    public void Total(){
-        return this.getProduk() + this.getOngkosKirim() + this.getBiayaPacking() + this.getAsuransi();
+    public String getCOD(){
+        return this.COD;
+    }
+
+    public void setCOD(String COD){
+        this.COD = COD;
+    }
+
+    public String getTransferBank(){
+        return this.TransferBank;
+    }
+
+    public void setTransferBank(String TransferBank){
+        this.TransferBank = TransferBank;
+    }
+
+    public double getTotal(){
+        return this.Total;
+    }
+
+    public void setTotal(double Total){
+        this.Total = Total;
     }
      
 
-=======
-public class struk_konfirmasi extends Transaksi {
-
-   private double Produk;
-   private double OngkosKirim;
-   private double BiayaPacking;
-   private double Asurasi;
-   private String idTransaksi;
-   private String idPembeli;
-   private int JumlahBarang;
-   private double HargaBarang;
-   private String COD;
-   private String TransferBank;
-
-   public class TotalBiaya{
-
-        public String Transaksi(){
-            return idTransaksi;
-            return idPembeli; 
-        }
-
-        public void Transaksi(String idTransaksi, String idPembeli){
-            this.idTransaksi = idTransaksi;
-            this.idPembeli = idPembeli;
-        }
-        public void getProduk(){
-            return Produk;
-        }
-
-        public void setProduk(double Produk){
-            this.Produk = Produk;
-        }
-
-        public void getBiayaPacking(){
-            return BiayaPacking;
-        }
-
-        public void setBiayaPacking(double BiayaPacking){
-            this.BiayaPacking = BiayaPacking;
-        }
-
-        public void getAsuransi(){
-            return Asuransi;
-        }
-
-        public void setAsurasi(double Asuransi){
-            this.Asuransi = Asuransi;
-        }
-
-        public Double Total(){
-            return this.getHargaBarang() * this.getJumlahBarang();
-        }
-    }
-
-    public class MetodePembayaran{
-
-        public String getCOD(){
-            return COD;
-        }
-
-        public void setCOD(String CoD){
-            this.COD = COD;
-        }
-
-        public String getTransferBank(){
-            return TransferBank;
-        }
-
-        public void setTransferBank(String TransferBank){
-            this.TransferBank = TransferBank;
-        }
-    } 
->>>>>>> Stashed changes
 }
