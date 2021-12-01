aeffb708cd53e5f67d7b3e3a22a542b7a5871ce9
RizkaNuzulia <81920773+RizkaNuzulia@users.noreply.github.com>
2021-12-01 22:20:56 +0700

    HalamanAwal.java
    Tampilan awal FIX

---
+++ HalamanAwal.java
@@ -0,0 +1,52 @@
+public class HalamanAwal{
+
+    private String opening;
+    private String gambarBarang;
+    protected double hargaBarang;
+    private String namaBarang;
+    private String ketBarang;
+
+    public HalamanAwal(){
+        this.opening = opening;
+    }
+
+    public HalamanAwal(String nama_barang, String gambar, String keterangan, double harga){
+        this.namaBarang = nama_barang;
+        this.gambarBarang = gambar;
+        this.ketBarang = keterangan;
+        this.hargaBarang = harga;
+    }
+   
+    public void setNamaBarang(String nama_barang){
+       this.namaBarang = nama_barang; 
+    }
+
+    public String getNamaBarang(){
+        return namaBarang;
+    }
+    
+    public void setGambarBarang(String gambar){
+        this.gambarBarang = gambar;
+    }
+
+    public String getGambarBarang(){
+        return gambarBarang;
+    }
+
+    public void setKetBarang(String keterangan){
+        this.ketBarang = keterangan;
+    } 
+
+    public String getKetBarang(){
+        return ketBarang;
+    }
+
+    public void setHargaBarang(double harga){
+        this.hargaBarang = harga;
+    }
+
+    public Double getHargaBarang(){
+       return hargaBarang;
+    }
+ 
+}
